0a8401b2ed3a7e1064db42f88fc60ede74db84db
KoffiDanhounsrou <koffi.danh@gmail.com>
2021-03-18 19:17:54 -0600

    uploading the demo

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
-# burger
-# burger
-# burger
-# burger
-# burger
-# burger
+
+  # Burger Logger
+  ![badge](https://img.shields.io/badge/License-MIT-brightgreen)
+ ## The Repository
+ This 'Burger Logger ' was created using MySQL, Node , Express, Handlebars and a ORM. It was created to log in your favorite burger that you wish to eat and then devours it after you ate one in really life. Burger Logger](https://limitless-hamlet-20887.herokuapp.com/)
+
+   ## Table of Contents
+   ### * [Installation](#installation)
+   ### * [Usage](#usage)
+   ### * [Credits](#credits)
+   ### * [License](#license)
+ 
+ ## Installation
+ To run this application, the user need fork the repo and then install the inquirer module by typing 'npm i" into the terminal.
+ ## Usage
+ Here is the step by step to run this application.
+
+ * All thew user got to do it to input the burger name and add. Then after click on the 'Devoured' button to move the name to devoured section.
+ 
+ 
+
+ ## Contribution
+ Collaborators: 
+ * Name: Koffi Danhounsrou
+ * [GitHub](https://github.com/Koffidanh)
+
+ ## Tests
+  No Test required. 
+## Video demo
+Here is the demo video.
+![demo](https://github.com/Koffidanh/burger/blob/main/assets/images/demo.gif)
+ ## License
+ Licensed under the MIT.
+  
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -4,58 +4,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
       console.info('DOM loaded');
     }
   
-    // UPDATE
-    const changeDevouredBtns = document.querySelectorAll('.change-devoured');
-  
-    // Set up the event listener for the create button
-    if (changeDevouredBtns) {
-      changeDevouredBtns.forEach((button) => {
-        button.addEventListener('click', (e) => {
-          // Grabs the id of the element that goes by the name, "id"
-          const id = e.target.getAttribute('data-id');
-          const newDevoured = e.target.getAttribute('data-newdevoured');
-  
-          const newDevouredState = {
-            devoured: newDevoured,
-          };
-  
-          fetch(`/api/burgers/${id}`, {
-            method: 'PUT',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-  
-            // make sure to serialize the JSON body
-            body: JSON.stringify(newDevouredState),
-          }).then((response) => {
-            // Check that the response is all good
-            // Reload the page so the user can see the new quote
-            if (response.ok) {
-              console.log(`changed devoured to: ${newDevoured}`);
-              location.reload('/');
-            } else {
-              alert('something went wrong!');
-            }
-          });
-        });
-      });
-    }
-  
+ 
     // CREATE
-    const createBurgerBtn = document.getElementById('create-form');
+    const createBurgerBtn = document.getElementById('create');
   
     if (createBurgerBtn) {
       createBurgerBtn.addEventListener('submit', (e) => {
         e.preventDefault();
   
-        // Grabs the value of the textarea that goes by the name, "quote"
+        // Grabs the value of the textarea that goes by the name
         const newBurger = {
-          burger_name: document.getElementById('bu').value.trim(),
-          //devoured: document.getElementById('devoured'),
+          burger_name: document.getElementById('burger').value.trim(),
+          
         };
   
-        // Send POST request to create a new quote
+        // Send POST request to create a new burger
         fetch('/api/burgers', {
           method: 'POST',
           headers: {
@@ -67,35 +30,52 @@ document.addEventListener('DOMContentLoaded', (event) => {
           body: JSON.stringify(newBurger),
         }).then(() => {
           // Empty the form
-          document.getElementById('bu').value = '';
+          document.getElementById('burger').value = '';
   
-          // Reload the page so the user can see the new quote
+          // Reload the page so the user can see the new burger
           console.log('Created a new Burger!');
           location.reload();
         });
       });
     }
+      // UPDATE
+      const changeDevouredBtns = document.querySelectorAll('.devoured');
   
-  // DELETE
-  const deleteBurgerBtns = document.querySelectorAll('.delete-burger');
-
-  // Set up the event listeners for each delete button
-  deleteBurgerBtns.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      const id = e.target.getAttribute('data-id');
-
-      // Send the delete request
-      fetch(`/api/burgers/${id}`, {
-        method: 'DELETE',
-      }).then((res) => {
-        console.log(res);
-        console.log(`Deleted burger: ${id}`);
-
-        // Reload the page
-        location.reload();
-      });
-    });
-  });
+      // Set up the event listener for the create button
+      if (changeDevouredBtns) {
+        changeDevouredBtns.forEach((button) => {
+          button.addEventListener('click', (e) => {
+            // Grabs the id of the element that goes by the name, "id"
+            const id = e.target.getAttribute('data-id');
+            const newDevoured = e.target.getAttribute('data-newdevoured');
+    
+            const newDevouredText = {
+              devoured: newDevoured,
+            };
+    
+            fetch(`/api/burgers/${id}`, {
+              method: 'PUT',
+              headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+              },
+    
+              // make sure to serialize the JSON body
+              body: JSON.stringify(newDevouredText),
+            }).then((response) => {
+              // Check that the response is all good
+              // Reload the page so the user can see the new burger
+              if (response.ok) {
+                console.log(`changed devoured to: ${newDevoured}`);
+                location.reload('/');
+              } else {
+                alert('something went wrong!');
+              }
+            });
+          });
+        });
+      }
+  
 });
 
 
